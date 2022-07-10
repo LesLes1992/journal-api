35b72da8b32795c08aec3ff31c881ba162ac13f5
Leslie-Gu <snowberg.prc@gmail.com>
2022-07-10 20:06:12 +1000

    udpated all the journal-api

--- index.js
+++ index.js
@@ -11,24 +11,44 @@ const app = express() //the req come in.
 const port = 8080
 
 mongoose.connect(
-    'mongodb+srv://cademo:cademo@cluster0.ga0axoz.mongodb.net/?retryWrites=true&w=majority')
+    'mongodb+srv://cademo:cademo@cluster0.ga0axoz.mongodb.net/journal?retryWrites=true&w=majority')
     .then(()=>console.log(mongoose.connection.readyState === 1 ? "Mongoose connected" : "Mongoose connected error"))
     .catch((error) => console.log(error.message))
 
+// create the schema by using new mongoose.Schema
+const entrySchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true
+    },
+    entry: {
+        type: String,
+        required: true
+    }
+})
+//create model base on the schema
+const EntryModel = mongoose.model('entry', entrySchema)
+
 app.use(express.json()) //app.use tells express execurte some middleware at this stage of the request-response cycle. In this case,we' re executing express.json(), 
 
-app.get('/entries/:id', (req,res) =>{res.send(entries[req.params.id])})
+app.get('/entries/:id', async (req,res) =>{
+    const entry = await EntryModel.findById(req.params.id)
+    res.send(entry)})
 
 app.get('/',(req,res) => res.send({info: 'journal API'})) // create a middleware and chaining them . the second parameter will be if the first one routes match running the second one
 
 app.get('/categories',(req,res) => console.log(req))
 
-app.get('/entries',(req,res) => res.send(entries))
+app.get('/entries',async (req,res) => {
+    const entries = await EntryModel.find()
+    res.send(entries)
+})
 
-app.post('/entries', (req,res) => {
+app.post('/entries', async (req,res) => {
     const entry = {category: req.body.category , entry: req.body.entry}
-    entries.push(entry)
-    res.status(201).send(entry)
+    // entries.push(entry)
+    const newEntry = await EntryModel.create(entry) // push the entry tp database,which create a new promise
+    res.status(201).send(newEntry)
 })
 
 // app.delete('/entries/:id', (req,res) =>{res.send(entries[req.params.id])})
